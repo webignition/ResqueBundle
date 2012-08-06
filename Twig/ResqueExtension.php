43f413c5c35df0f6b1cdb55a1efd1211408f88f6
William POTTIER <wpottier@clever-age.com>
2012-08-06 10:50:50 +0200

    remove external reference to twig debug filter

---
+++ Twig/ResqueExtension.php
@@ -0,0 +1,107 @@
+<?php
+namespace Glit\ResqueBundle\Twig;
+
+class ResqueExtension extends \Twig_Extension {
+
+    /**
+     * Returns the name of the extension.
+     *
+     * @return string The extension name
+     */
+    public function getName() {
+        return 'glit_resque';
+    }
+
+    public function getFilters() {
+        return array(
+            'resque_relativeDate'  => new \Twig_Filter_Method($this, 'relativeDate'),
+            'resque_localeDate'    => new \Twig_Filter_Method($this, 'localeDate'),
+
+            'resque_trim'          => new \Twig_Filter_Method($this, 'trim'),
+
+            'resque_debug'         => new \Twig_Filter_Method($this, 'debug'),
+            'resque_type'          => new \Twig_Filter_Method($this, 'type'),
+        );
+    }
+
+    /**
+     * Create relative from today text representation of a date
+     * @param $time
+     * @return string
+     * TODO : internationalize method
+     */
+    public function relativeDate($time) {
+        $today = strtotime(date('M j, Y'));
+
+        if ($time instanceof \DateTime) {
+            $time = $time->getTimestamp();
+        }
+
+        $reldays = ($time - $today) / 86400;
+
+        if ($reldays >= 0 && $reldays < 1) {
+            return 'today';
+        }
+        else if ($reldays >= 1 && $reldays < 2) {
+            return 'tomorrow';
+        }
+        else if ($reldays >= -1 && $reldays < 0) {
+            return 'yesterday';
+        }
+
+        if (abs($reldays) < 7) {
+            if ($reldays > 0) {
+                $reldays = floor($reldays);
+
+                return 'in ' . $reldays . ' day' . ($reldays != 1 ? 's' : '');
+            }
+            else {
+                $reldays = abs(floor($reldays));
+
+                return $reldays . ' day' . ($reldays != 1 ? 's' : '') . ' ago';
+            }
+        }
+
+        if (abs($reldays) < 182) {
+            return date('l, j F', $time ? $time : time());
+        }
+        else {
+            return date('l, j F, Y', $time ? $time : time());
+        }
+    }
+
+    public function localeDate($date, $dateType = 'medium', $timeType = 'none') {
+        $values       = array(
+            'none'   => \IntlDateFormatter::NONE,
+            'short'  => \IntlDateFormatter::SHORT,
+            'medium' => \IntlDateFormatter::MEDIUM,
+            'long'   => \IntlDateFormatter::LONG,
+            'full'   => \IntlDateFormatter::FULL,
+        );
+        $dateFormater = \IntlDateFormatter::create(
+            \Locale::getDefault(),
+            isset($values[$dateType]) ? $values[$dateType] : $dateType,
+            isset($values[$timeType]) ? $values[$timeType] : $timeType,
+            date_default_timezone_get(),
+            \IntlDateFormatter::GREGORIAN
+        );
+        return $dateFormater->format($date->getTimestamp());
+    }
+
+    public function debug($data) {
+        return '<pre>' . print_r($data, true) . '</pre>';
+    }
+
+    public function type($data) {
+        if (is_object($data)) {
+            return get_class($data);
+        }
+        else {
+            return gettype($data);
+        }
+    }
+
+    public function trim($text, $charList = null) {
+        return trim($text, $charList);
+    }
+}
